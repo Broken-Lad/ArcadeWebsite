46cc254c4462dc21cdf619dc23f0d5a02cd182d1
Broken-Lad <turan@suslu.eu>
2024-03-27 11:26:59 +0100

    initial commit
    Added:
    Html
    Js
    img

---
+++ node.js
@@ -0,0 +1,26 @@
+function Start(){
+    document.body.style.backgroundImage = "url('./media/img/arcade.jpg')";
+}
+
+
+function VampireSavior2(){
+    document.getElementById("gameScreen").innerHTML = '<iframe src="https://www.retrogames.cc/embed/10590-vampire-savior-the-lord-of-vampire-970519-usa.html" width="1200"height="900" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"scrolling="no"></iframe>';
+    document.body.style.backgroundImage = "url('./media/img/vampiresavior2.jpg')";
+}
+
+
+function StreetFighterAlpha2(){
+    document.getElementById("gameScreen").innerHTML = '<iframe src="https://www.retrogames.cc/embed/9967-street-fighter-alpha-2-960430-usa.html" width="1200" height="900" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe>';
+    document.body.style.backgroundImage = "url('./media/img/sfa2.jpg')";
+}
+
+
+function FinalFight(){
+    document.getElementById("gameScreen").innerHTML = '<iframe src="https://www.retrogames.cc/embed/8497-final-fight-900112-usa.html" width="1200" height="900" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe>';
+    document.body.style.backgroundImage = "url('./media/img/finalFight.jpg')";
+}
+
+function TurtlesInTime(){
+    document.getElementById("gameScreen").innerHTML = '<iframe src="https://www.retrogames.cc/embed/10271-teenage-mutant-ninja-turtles-turtles-in-time-4-players-ver-uaa.html" width="1200" height="900" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe>';
+    document.body.style.backgroundImage = "url('./media/img/tmnt.jpg')";
+}
